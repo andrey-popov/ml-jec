bbeb097978e49a3c2fceae4a3b2e5e46ce7fc092
Andrey Popov <rainfinder@gmail.com>
2020-08-18 00:39:32 +0200

    Generalize function to construct MLP and ResNet
    Prepare to use them not only for the head but also in application to
    individual constituents. Also provide better names for their layers and
    annotate types in model.

--- mljec/model.py
+++ mljec/model.py
@@ -1,13 +1,15 @@
 from collections.abc import Sequence
 import math
+from typing import List, Mapping
 
+import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.layers import (
     Activation, Add, BatchNormalization, Dense, Dropout
 )
 
 
-def build_model(config):
+def build_model(config: Mapping) -> keras.Model:
     """Construct model specified in the configuration.
 
     Also create optimizer and set the loss function.
@@ -30,19 +32,20 @@ def build_model(config):
     head_config = model_config['head']
     inputs = keras.Input(shape=(num_features, ), name='global_numeric')
     model_type = head_config['type']
-    if model_type == 'plain':
+    assert head_config['num_units'][-1] == 1
+    if model_type == 'mlp':
         out = _apply_mlp(
             inputs,
-            num_layers=head_config.get('num_layers', None),
             num_units=head_config['num_units'],
             batch_norm=head_config.get('batch_norm', False),
-            dropout=head_config.get('dropout', 0)
+            dropout=head_config.get('dropout', 0),
+            name_prefix='head_'
         )
     elif model_type == 'resnet':
         out = _apply_resnet(
             inputs,
-            num_layers=head_config['num_layers'],
-            num_units=head_config['num_units']
+            num_units=head_config['num_units'],
+            name_prefix='head_'
         )
     else:
         raise RuntimeError(f'Unknown model type "{model_type}".')
@@ -58,65 +61,94 @@ def build_model(config):
 
 
 def _apply_mlp(
-    inputs, num_layers=5, num_units=256, batch_norm=False, dropout=0
-):
+    inputs: tf.Tensor, num_units: List[int], batch_norm: bool = False,
+    dropout: float = 0., name_prefix: str = ''
+) -> tf.Tensor:
     """Apply an MLP fragment to given inputs.
 
     Args:
         inputs:  Inputs to the network fragment.
-        num_layers:  Number of hidden layers.  Ignored if num_units is
-            a sequence.
-        num_units:  Number of units in each hidden layer.  Can be a
-            sequence.  In that case it provides numbers of units in all
-            hidden layers.
-        batch_norm:  Whether to apply batch normalization.
-        dropout:  Dropout rate.  Disabled if 0.
+        num_units:  Numbers of units in each layer.
+        batch_norm:  Whether to apply batch normalization.  Never
+            applied in the last layer.
+        dropout:  Dropout rate.  Disabled if 0.  Never applied in the
+            last layer.
+        name_prefix:  Name prefix for layers.
     """
 
-    if not isinstance(num_units, Sequence):
-        num_units = [num_units] * num_layers
-
     x = inputs
-    for i, n in enumerate(num_units):
+    for layer_index, layer_num_units in enumerate(num_units):
+        is_last_layer = layer_index == len(num_units) - 1
         x = Dense(
-            n, kernel_initializer='he_uniform', use_bias=not batch_norm
+            layer_num_units, kernel_initializer='he_uniform',
+            use_bias=not batch_norm or is_last_layer,
+            name=name_prefix + f'dense_{layer_index + 1}'
         )(x)
-        if batch_norm:
-            x = BatchNormalization(scale=False)(x)
-        x = Activation('relu')(x)
-        if dropout > 0 and i != 0:
-            x = Dropout(dropout)(x)
-    x = Dense(1, kernel_initializer='he_uniform')(x)
+        if batch_norm and not is_last_layer:
+            x = BatchNormalization(
+                scale=False, name=name_prefix + f'batch_norm_{layer_index + 1}'
+            )(x)
+        x = Activation(
+            'relu', name=name_prefix + f'activation_{layer_index + 1}'
+        )(x)
+        if dropout > 0. and not is_last_layer:
+            x = Dropout(
+                dropout, name=name_prefix + f'dropout_{layer_index + 1}'
+            )(x)
     return x
 
 
-def _apply_resnet(inputs, num_layers=5, num_units=256):
-    """Apply a ResNet fragment to inputs.
+def _apply_resnet(
+    inputs: tf.Tensor, num_units: List[int],
+    name_prefix: str = ''
+) -> tf.Tensor:
+    """Apply a ResNet fragment to given inputs.
 
     Args:
         inputs:  Inputs for the network fragment.
-        num_layers:  Number of ResNet blocks.  Each block includes two
-            sublayers with non-linearities.
-        num_units:  Number of units in each hidden layer.
+        num_units:  Numbers of units in each layer.
+        name_prefix:  Name prefix for layers.
+
+    The skip connections will include trainable projections if the
+    dimensions of layers with odd indices don't match.
     """
 
-    # The first layer is special in that it includes a projection for
-    # the inputs in order to match the dimensions
-    x = Dense(
-        num_units, kernel_initializer='he_uniform', use_bias=False
-    )(inputs)
-    y = Dense(num_units, kernel_initializer='he_uniform')(x)
-    y = Activation('relu')(y)
-    y = Dense(num_units, kernel_initializer='he_uniform')(y)
-    y = Add()([x, y])
-    x = Activation('relu')(y)
+    x = inputs
 
-    for _ in range(num_layers - 1):
-        y = Dense(num_units, kernel_initializer='he_uniform')(x)
-        y = Activation('relu')(y)
-        y = Dense(num_units, kernel_initializer='he_uniform')(y)
-        y = Add()([x, y])
-        x = Activation('relu')(y)
+    for i in range(0, len(num_units) - 1, 2):
+        n1 = num_units[i]
+        n2 = num_units[i + 1]
+        ilayer = i // 2 + 1
 
-    output = Dense(1, kernel_initializer='he_uniform')(x)
-    return output
+        x_main = Dense(
+            n1, kernel_initializer='he_uniform',
+            name=name_prefix + f'dense_{ilayer}_1'
+        )(x)
+        x_main = Activation(
+            'relu', name=name_prefix + f'activation_{ilayer}_1'
+        )(x_main)
+        x_main = Dense(
+            n2, kernel_initializer='he_uniform',
+            name=name_prefix + f'dense_{ilayer}_2'
+        )(x_main)
+
+        if x.shape[-1] == n2:
+            x_bypass = x
+        else:
+            # Include a projection to match the dimensions
+            x_bypass = Dense(
+                n2, kernel_initializer='he_uniform', use_bias=False,
+                name=name_prefix + f'projection_{ilayer}'
+            )(x)
+
+        x = Add(name=name_prefix + f'add_{ilayer}')([x_main, x_bypass])
+        x = Activation('relu', name=name_prefix + f'activation_{ilayer}_2')(x)
+
+    if len(num_units) % 2 == 1:
+        x = Dense(
+            num_units[-1], kernel_initializer='he_uniform',
+            name=name_prefix + 'dense_last'
+        )(x)
+        x = Activation('relu', name=name_prefix + 'activation_last')(x)
+
+    return x
